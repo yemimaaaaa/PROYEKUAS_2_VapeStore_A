4c8b14a5d75756beb08dddf58352744f003f01af
yemimaaaaa <yemimahah@gmail.com>
2024-05-24 22:47:59 +0700

    YH

---
+++ app/ui/fonts.tsx
@@ -0,0 +1,34 @@
+import { Kanit, Anton, Lusitana, Inter, Rowdies, Montserrat} from 'next/font/google';
+ 
+export const kanit = Kanit({
+    weight: ['400'],
+    subsets: ['latin'],
+  });
+
+  export const anton = Anton({
+    weight: ['400'],
+    subsets: ['latin'],
+  })
+  
+  export const lusitana = Lusitana({
+    weight: ['400', '700'],
+    subsets: ['latin'],
+  }
+  );
+
+  export const inter = Inter({
+    weight: ['400', '700'],
+    subsets: ['latin'],
+  }
+  );
+
+  export const rowdies = Rowdies({
+    weight: ['400', '700'],
+    subsets: ['latin'],
+  }
+  );
+
+  export const montserrat = Montserrat ({
+    weight: ['500'],
+    subsets: ['latin'],
+  });
