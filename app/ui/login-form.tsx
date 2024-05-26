1c9b2b3fdf4ba317122492ac63323565f191cfc8
yemimaaaaa <yemimahah@gmail.com>
2024-05-26 14:49:03 +0700

    yy

--- app/ui/login-form.tsx
+++ app/ui/login-form.tsx
@@ -9,7 +9,7 @@ import {
 import { ArrowRightIcon } from '@heroicons/react/20/solid';
 import { Button } from './button';
 import { useFormState, useFormStatus } from 'react-dom';
-import { authenticate } from '@/app/lib/action';
+import { authenticate } from '@/app/lib/actions';
 
 export default function LoginForm() {
   const [errorMessage, dispatch] = useFormState(authenticate, undefined);
